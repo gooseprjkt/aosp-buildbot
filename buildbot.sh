77290c4429e5eb79bfb59985446f73ec8dedb055
gooseprjkt <162053558+gooseprjkt@users.noreply.github.com>
2024-03-19 19:31:20 +0300

    Update buildbot.sh

--- buildbot.sh
+++ buildbot.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+echo Chmod-ding all scripts
+chmod +x config.sh
+chmod +x enviroment.sh
+chmod +x sync.sh
+chmod +x buid.sh
+
+echo Running config script
+bash config.sh
+
 echo Running enviroment setup script
 bash enviroment.sh
 
