55cfd4c4c92bdc4754e3e016dca1e922fa0d4f92
gooseprjkt <162053558+gooseprjkt@users.noreply.github.com>
2024-03-19 18:24:56 +0300

    Update enviroment.sh

--- enviroment.sh
+++ enviroment.sh
@@ -1,1 +1,6 @@
+#!/bin/bash
 
+echo Installing packages
+apt-get install bc bison build-essential ccache curl flex g++-multilib gcc-multilib git git-lfs gnupg gperf imagemagick lib32readline-dev lib32z1-dev libelf-dev liblz4-tool libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev
+apt-get install lib32ncurses5-dev libncurses5 libncurses5-dev
+apt-get install gcc-multilib g++-multilib libssl-dev clang clang-tools
