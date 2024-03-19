c3f35fd18957a78ffdfb51e1659b26b9c348411c
gooseprjkt <162053558+gooseprjkt@users.noreply.github.com>
2024-03-19 19:56:24 +0300

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -3,15 +3,15 @@
 echo Downloading sources
 mkdir making-zone && cd making-zone
 
-repo init https://github.com/Halium/android.git -b halium-11.0
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j32
+repo init -u "$MANIFEST_URL" -b "$MANIFEST_BRANCH"
+repo sync "$SYNC_OPTIONS"
 
 echo Downloading proprietary repositories
-mkdir device/xiaomi
-mkdir vendor/xiaomi
-mkdir kernel/xiaomi
-git clone https://github.com/crdroidandroid/android_device_xiaomi_chime.git -b 11.0 device/xiaomi/chime
-git clone https://github.com/KiranPranay/vendor_xiaomi_juice.git -b 11 vendor/xiaomi/chime
-git clone https://github.com/crdroidandroid/android_kernel_xiaomi_chime.git -b 13.0 kernel/xiaomi/chime
+mkdir device/"$DEVICE_VENDOR"
+mkdir vendor/"$DEVICE_VENDOR"
+mkdir kernel/"$DEVICE_VENDOR"
+git clone "$KERNEL_REPO_URL" -b "$KERNEL_REPO_BRANCH" kernel/"$DEVICE_VENDOR"/"$DEVICE_CODENAME"
+git clone "$VENDOR_REPO_URL" -b "$VENDOR_REPO_BRANCH" vendor/"$DEVICE_VENDOR"/"$DEVICE_CODENAME"
+git clone "$DEVICE_TREE_REPO_URL" -b "$DEVICE_TREE_REPO_BRANCH" device/"$DEVICE_VENDOR"/"$DEVICE_CODENAME"
 
 echo Sync successful
